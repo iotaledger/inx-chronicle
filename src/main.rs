d5f0e7dfa98e030bc2cb614e7c2cd7766c27b354
Jochen Görtler <jochen.goertler@iota.org>
2022-03-30 15:25:40 +0200

    :tada: Initial version

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,29 @@
 
 #![warn(missing_docs)]
 
-//! TODO
+use futures::stream::StreamExt;
+use inx::{client::InxClient, proto::NoParams};
+use tonic::transport::Channel;
 
-fn main() {
-    println!("Hello world from Chronicle!");
+async fn milestones(client: &mut InxClient<Channel>, num: usize) {
+    let response = client.listen_to_latest_milestone(NoParams {}).await;
+    println!("{:#?}", response);
+    let stream = response.unwrap().into_inner();
+
+    // stream is infinite - take just `num` elements and then disconnect
+    let mut stream = stream.take(num);
+    while let Some(item) = stream.next().await {
+        println!("\trecived: {:#?}", item.unwrap());
+    }
+    // stream is droped here and the disconnect info is send to server
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Start Hornet with `./hornet --inx.bindAddress=127.0.0.1:50001 --deleteAll --config=config.json`
+    let mut client = InxClient::connect("http://127.0.0.1:50001").await?;
+
+    milestones(&mut client, 5).await;
+
+    Ok(())
 }
