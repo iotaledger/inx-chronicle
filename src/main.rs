ced3c84ed6d7355d230d07bcef1d4552a1b9ab5e
Jochen Görtler <jochen.goertler@iota.org>
2022-04-01 22:51:40 +0200

    :construction: WIP

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,76 @@
 
 #![warn(missing_docs)]
 
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::get,
+    Extension, Router,
+};
 use futures::stream::StreamExt;
 use inx::{client::InxClient, proto::NoParams};
+use tokio::sync::RwLock;
 use tonic::transport::Channel;
 
-async fn milestones(client: &mut InxClient<Channel>, num: usize) {
+#[derive(Debug)]
+struct Error;
+
+async fn root(Extension(handl): Extension<Arc<RwLock<i32>>>) -> Result<impl IntoResponse, Error> {
+    let v = (*handl).read().await;
+    Ok(format!("Milestone count: {:#?}", v))
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        (StatusCode::FORBIDDEN, "Bad bad bad").into_response()
+    }
+}
+
+async fn milestones(client: &mut InxClient<Channel>, lock: Arc<RwLock<i32>>) {
     let response = client.listen_to_latest_milestone(NoParams {}).await;
     println!("{:#?}", response);
-    let stream = response.unwrap().into_inner();
+    let mut stream = response.unwrap().into_inner();
 
-    // stream is infinite - take just `num` elements and then disconnect
-    let mut stream = stream.take(num);
     while let Some(item) = stream.next().await {
         println!("\trecived: {:#?}", item.unwrap());
+        let mut counter = lock.write().await;
+        *counter += 1;
     }
     // stream is droped here and the disconnect info is send to server
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Start Hornet with `./hornet --inx.bindAddress=127.0.0.1:50001 --deleteAll --config=config.json`
-    let mut client = InxClient::connect("http://127.0.0.1:50001").await?;
+    let lock = Arc::new(RwLock::new(5));
+    let c_lock = lock.clone();
 
-    milestones(&mut client, 5).await;
+    tokio::spawn(async move {
+        // build our application with a route
+        let app = Router::new()
+            // `GET /` goes to `root`
+            .route("/milestones", get(root))
+            .layer(Extension(c_lock.clone()));
+
+        // run our app with hyper
+        // `axum::Server` is a re-export of `hyper::Server`
+        let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+        println!("listening on {}", addr);
+        axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+    });
+
+    println!("Wait for INX");
+
+    tokio::spawn(async move {
+        let mut client = InxClient::connect("http://localhost:9029").await.unwrap();
+
+        milestones(&mut client, lock).await;
+
+    });
+
+    loop {}
 
     Ok(())
 }
