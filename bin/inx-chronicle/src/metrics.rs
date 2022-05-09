ebb8d9ba6c0fcc767078216b44a0e5b57e987be9
Christian Poveda <christian.poveda@iota.org>
2022-05-09 14:59:25 -0500

    handle shutdown

--- bin/inx-chronicle/src/metrics.rs
+++ bin/inx-chronicle/src/metrics.rs
@@ -6,8 +6,8 @@ use std::{convert::Infallible, sync::Arc};
 use async_trait::async_trait;
 use bee_metrics::{encoding::SendSyncEncodeMetric, metrics::ProcessMetrics, serve_metrics, Registry};
 use chronicle::runtime::{Actor, ActorContext, HandleEvent};
-use futures::future::FutureExt;
 use tokio::{
+    sync::oneshot,
     task::JoinHandle,
     time::{sleep, Duration},
 };
@@ -15,19 +15,23 @@ use tokio::{
 #[derive(Default)]
 pub struct MetricsWorker {}
 
+pub struct MetricsState {
+    registry: Arc<Registry>,
+    server_handle: (JoinHandle<()>, Option<oneshot::Sender<()>>),
+    process_metrics_handle: JoinHandle<()>,
+}
+
 #[async_trait]
 impl Actor for MetricsWorker {
-    type State = (Arc<Registry>, JoinHandle<()>, JoinHandle<()>);
+    type State = MetricsState;
 
     type Error = Infallible;
 
-    async fn init(&mut self, _cx: &mut ActorContext<Self>) -> Result<Self::State, Self::Error> {
+    async fn init(&mut self, cx: &mut ActorContext<Self>) -> Result<Self::State, Self::Error> {
         let registry = Arc::new(Registry::default());
 
         // FIXME: pass address via config.
         let addr = "0.0.0.0:6969".parse().unwrap();
-
-        let server_fut = tokio::spawn(serve_metrics(addr, registry.clone()).map(|res| res.unwrap()));
 
         let metrics = ProcessMetrics::new(std::process::id());
         let (mem_metric, cpu_metric) = metrics.metrics();
@@ -35,7 +39,29 @@ impl Actor for MetricsWorker {
         registry.register("memory_usage", "Memory usage", mem_metric);
         registry.register("cpu_usage", "CPU usage", cpu_metric);
 
-        let process_metrics_fut = {
+        let (send, recv) = oneshot::channel();
+        let metrics_handle = cx.handle().clone();
+
+        let server_fut = {
+            let registry = registry.clone();
+            tokio::spawn(async move {
+                let fut = serve_metrics(addr, registry);
+
+                // FIXME: use `with_graceful_shutdown` when `bee-metrics` exposes the complete
+                // future type.
+                let res = tokio::select! {
+                    res = fut => { res }
+                    _ = recv => { Ok(()) }
+                };
+
+                // Stop the actor if the server stops.
+                metrics_handle.shutdown();
+
+                res.unwrap()
+            })
+        };
+
+        let process_metrics_handle = {
             tokio::spawn(async move {
                 loop {
                     metrics.update().await;
@@ -44,20 +70,19 @@ impl Actor for MetricsWorker {
             })
         };
 
-        Ok((registry, server_fut, process_metrics_fut))
+        Ok(MetricsState {
+            registry,
+            server_handle: (server_fut, Some(send)),
+            process_metrics_handle,
+        })
     }
 
-    async fn shutdown(
-        &mut self,
-        cx: &mut ActorContext<Self>,
-        (_, server_fut, process_metrics_fut): &mut Self::State,
-    ) -> Result<(), Self::Error> {
+    async fn shutdown(&mut self, cx: &mut ActorContext<Self>, state: &mut Self::State) -> Result<(), Self::Error> {
         log::debug!("{} shutting down ({})", self.name(), cx.id());
 
-        process_metrics_fut.abort();
+        state.process_metrics_handle.abort();
 
-        // FIXME: is this good enough to stop the task?
-        server_fut.abort();
+        state.server_handle.1.take().map(|send| send.send(()));
 
         Ok(())
     }
@@ -75,9 +100,9 @@ impl<M: 'static + SendSyncEncodeMetric> HandleEvent<RegisterMetric<M>> for Metri
         &mut self,
         _cx: &mut ActorContext<Self>,
         event: RegisterMetric<M>,
-        (registry, _, _): &mut Self::State,
+        state: &mut Self::State,
     ) -> Result<(), Self::Error> {
-        registry.register(event.name, event.help, event.metric);
+        state.registry.register(event.name, event.help, event.metric);
 
         Ok(())
     }
