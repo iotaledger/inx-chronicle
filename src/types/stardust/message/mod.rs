95e79f58728c262efd5cd7e54800b03c744466cc
Jochen Görtler <jochen.goertler@iota.org>
2022-05-18 20:20:38 +0200

    Pull out `MessageId`

--- src/types/stardust/message/mod.rs
+++ src/types/stardust/message/mod.rs
@@ -3,49 +3,16 @@
 
 mod address;
 mod input;
+mod message_id;
 mod output;
 mod payload;
 mod signature;
 mod unlock_block;
 
-use std::str::FromStr;
-
 use bee_message_stardust as bee;
 use serde::{Deserialize, Serialize};
 
-pub use self::{address::*, input::*, output::*, payload::*, signature::*, unlock_block::*};
-
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Hash, Ord, PartialOrd, Eq)]
-#[serde(transparent)]
-pub struct MessageId(#[serde(with = "serde_bytes")] pub Box<[u8]>);
-
-impl MessageId {
-    pub fn to_hex(&self) -> String {
-        prefix_hex::encode(self.0.as_ref())
-    }
-}
-
-impl From<bee::MessageId> for MessageId {
-    fn from(value: bee::MessageId) -> Self {
-        Self(value.to_vec().into_boxed_slice())
-    }
-}
-
-impl TryFrom<MessageId> for bee::MessageId {
-    type Error = crate::types::error::Error;
-
-    fn try_from(value: MessageId) -> Result<Self, Self::Error> {
-        Ok(bee::MessageId::new(value.0.as_ref().try_into()?))
-    }
-}
-
-impl FromStr for MessageId {
-    type Err = crate::types::error::ParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(bee::MessageId::from_str(s)?.into())
-    }
-}
+pub use self::{address::*, input::*, message_id::*, output::*, payload::*, signature::*, unlock_block::*};
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Message {
