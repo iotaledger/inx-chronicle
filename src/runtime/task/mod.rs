fe00f3e8ede209d334af9049e111e1e59401692b
Christian Poveda <christian.poveda@iota.org>
2022-05-25 12:53:53 -0500

    remove `Sync` bound for `Task::Error`

--- src/runtime/task/mod.rs
+++ src/runtime/task/mod.rs
@@ -10,7 +10,7 @@ use async_trait::async_trait;
 
 #[async_trait]
 pub trait Task: Send + Sync + Sized {
-    type Error: Error + Send + Sync;
+    type Error: Error + Send;
 
     /// Set this task's name, primarily for debugging purposes.
     fn name(&self) -> Cow<'static, str> {
