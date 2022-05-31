2e3ea2341ad8e540f10088ca4c36269b12928121
Christian Poveda <christian.poveda@iota.org>
2022-05-31 13:21:05 -0500

    add global lock for tests using `mongodb`

--- tests/block.rs
+++ tests/block.rs
@@ -1,15 +1,45 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use std::ops::Deref;
+
 use bee_test::rand::block::{rand_block, rand_block_id};
 use chronicle::{
-    db::{MongoDb, MongoDbConfig},
+    db::MongoDbConfig,
     types::{
         ledger::{BlockMetadata, ConflictReason, LedgerInclusionState},
         stardust::block::{Block, BlockId},
     },
 };
+use once_cell::sync::Lazy;
 use packable::PackableExt;
+use tokio::sync::{Mutex, MutexGuard};
+
+static DB_LOCK: Lazy<Mutex<()>> = Lazy::new(|| Mutex::new(()));
+
+struct MongoDb<'a> {
+    db: chronicle::db::MongoDb,
+    _guard: MutexGuard<'a, ()>,
+}
+
+impl<'a> MongoDb<'a> {
+    async fn connect(config: &MongoDbConfig) -> Result<MongoDb<'a>, mongodb::error::Error> {
+        let _guard = DB_LOCK.lock().await;
+
+        let db = chronicle::db::MongoDb::connect(config).await?;
+        db.clear().await?;
+
+        Ok(Self { db, _guard })
+    }
+}
+
+impl<'a> Deref for MongoDb<'a> {
+    type Target = chronicle::db::MongoDb;
+
+    fn deref(&self) -> &Self::Target {
+        &self.db
+    }
+}
 
 #[ignore]
 #[tokio::test]
@@ -34,8 +64,6 @@ async fn insert_and_get_block() -> Result<(), mongodb::error::Error> {
 
     let config = MongoDbConfig::default().with_suffix("cargo-test");
     let db = MongoDb::connect(&config).await?;
-
-    db.clear().await?;
 
     db.insert_block_with_metadata(block_id, block, raw, metadata, 0).await?;
 
