24983a7754a96236551bef1f98d99e245ea5876c
Alexandcoats <alexandcoats@gmail.com>
2022-06-10 11:19:41 -0400

    privatify block document

--- src/db/collections/block.rs
+++ src/db/collections/block.rs
@@ -20,18 +20,18 @@ use crate::{
 
 /// Chronicle Block record.
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct BlockDocument {
+struct BlockDocument {
     /// The id of the current block.
-    pub block_id: BlockId,
+    block_id: BlockId,
     /// The block.
-    pub block: Block,
+    block: Block,
     /// The raw bytes of the block.
     #[serde(with = "serde_bytes")]
-    pub raw: Vec<u8>,
+    raw: Vec<u8>,
     /// The block's metadata.
-    pub metadata: BlockMetadata,
+    metadata: BlockMetadata,
     /// The index of this block in white flag order.
-    pub white_flag_index: u32,
+    white_flag_index: u32,
 }
 
 impl BlockDocument {
@@ -312,18 +312,26 @@ impl MongoDb {
     }
 
     /// Gets the spending transaction of an [`Output`] by [`OutputId`].
-    pub async fn get_spending_transaction(&self, output_id: &OutputId) -> Result<Option<BlockDocument>, Error> {
-        self.0
-            .collection::<BlockDocument>(BlockDocument::COLLECTION)
-            .find_one(
-                doc! {
-                    "metadata.inclusion_state": LedgerInclusionState::Included,
-                    "block.payload.essence.inputs.transaction_id": &output_id.transaction_id,
-                    "block.payload.essence.inputs.index": &(output_id.index as i32)
-                },
+    pub async fn get_spending_transaction(&self, output_id: &OutputId) -> Result<Option<Block>, Error> {
+        Ok(self
+            .0
+            .collection::<Block>(BlockDocument::COLLECTION)
+            .aggregate(
+                vec![
+                    doc! { "$match": {
+                        "metadata.inclusion_state": LedgerInclusionState::Included,
+                        "block.payload.essence.inputs.transaction_id": &output_id.transaction_id,
+                        "block.payload.essence.inputs.index": &(output_id.index as i32)
+                    } },
+                    doc! { "$replaceRoot": { "newRoot": "$block" } },
+                ],
                 None,
             )
-            .await
+            .await?
+            .try_next()
+            .await?
+            .map(bson::from_document)
+            .transpose()?)
     }
 
     /// Gets the spending transaction metadata of an [`Output`] by [`OutputId`].
