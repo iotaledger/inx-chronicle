8af160f32659f3fe15c65a98dc96e921ef51b75f
Alexandcoats <alexandcoats@gmail.com>
2022-06-21 02:35:28 -0400

    feat(analytics): add transaction analytics (#292)
    * Add transaction analytics
    
    * Cleanup queries
    
    * clippy
    
    * Use toDouble to calculate analytics in query

--- bin/inx-chronicle/src/api/stardust/analytics/routes.rs
+++ bin/inx-chronicle/src/api/stardust/analytics/routes.rs
@@ -4,11 +4,13 @@
 use axum::{routing::get, Extension, Router};
 use chronicle::db::MongoDb;
 
-use super::responses::AddressAnalyticsResponse;
+use super::responses::{AddressAnalyticsResponse, TransactionsAnalyticsResponse};
 use crate::api::{extractors::TimeRange, ApiError, ApiResult};
 
 pub fn routes() -> Router {
-    Router::new().route("/addresses", get(address_analytics))
+    Router::new()
+        .route("/addresses", get(address_analytics))
+        .route("/transactions", get(transaction_analytics))
 }
 
 async fn address_analytics(
@@ -18,6 +20,25 @@ async fn address_analytics(
         end_timestamp,
     }: TimeRange,
 ) -> ApiResult<AddressAnalyticsResponse> {
+    let res = database
+        .get_address_analytics(start_timestamp, end_timestamp)
+        .await?
+        .ok_or(ApiError::NoResults)?;
+
+    Ok(AddressAnalyticsResponse {
+        total_addresses: res.total_addresses,
+        recv_addresses: res.recv_addresses,
+        send_addresses: res.send_addresses,
+    })
+}
+
+async fn transaction_analytics(
+    database: Extension<MongoDb>,
+    TimeRange {
+        start_timestamp,
+        end_timestamp,
+    }: TimeRange,
+) -> ApiResult<TransactionsAnalyticsResponse> {
     let start_milestone = database
         .find_first_milestone(start_timestamp)
         .await?
@@ -28,13 +49,12 @@ async fn address_analytics(
         .ok_or(ApiError::NoResults)?;
 
     let res = database
-        .aggregate_addresses(start_milestone, end_milestone)
-        .await?
-        .ok_or(ApiError::NoResults)?;
+        .get_transaction_analytics(start_milestone, end_milestone)
+        .await?;
 
-    Ok(AddressAnalyticsResponse {
-        total_addresses: res.total_addresses,
-        recv_addresses: res.recv_addresses,
-        send_addresses: res.send_addresses,
+    Ok(TransactionsAnalyticsResponse {
+        count: res.count,
+        total_value: res.total_value,
+        avg_value: res.avg_value,
     })
 }
