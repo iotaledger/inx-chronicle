924c43ea54487f17774a60358de4316a8855c001
Alexander Schmidt <alexander.schmidt@iota.org>
2022-07-14 14:52:00 +0200

    Remove one cfg

--- bin/inx-chronicle/src/api/routes.rs
+++ bin/inx-chronicle/src/api/routes.rs
@@ -62,6 +62,9 @@ async fn login(
 }
 
 async fn is_healthy(database: Extension<MongoDb>) -> bool {
+    #[allow(unused_mut)]
+    let mut result = true;
+
     #[cfg(feature = "stardust")]
     {
         let end = match database.get_latest_milestone().await {
@@ -77,13 +80,13 @@ async fn is_healthy(database: Extension<MongoDb>) -> bool {
         }
 
         // Check if there are no gaps in the sync status.
-        match database.get_gaps().await {
+        result = match database.get_gaps().await {
             Ok(gaps) => gaps.is_empty(),
             _ => false,
-        }
-    }
-    #[cfg(not(feature = "stardust"))]
-    true
+        };
+    };
+
+    result
 }
 
 pub async fn info(database: Extension<MongoDb>) -> InfoResponse {
