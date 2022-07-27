2e489e0a2efad96968a02c816993d315b386a01c
Jochen Görtler <jochen.goertler@iota.org>
2022-07-27 14:38:32 +0200

    update patch

--- docker/update_hornet_config.sh
+++ docker/update_hornet_config.sh
@@ -4,4 +4,4 @@ DIR=$(dirname ${BASH_SOURCE[0]})
 wget https://raw.githubusercontent.com/gohornet/hornet/develop/config_alphanet.json -O ${DIR}/config.alphanet.hornet.json
 
 # We apply a patch to enable INX
-git apply docker/hornet_config.patch
+patch ${DIR}/config.hornet.json < ${DIR}/hornet_config.patch
