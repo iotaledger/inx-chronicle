00f3c66e3dfd28d85500c7930588660f5134b181
Alexander Schmidt <alexander.schmidt@iota.org>
2022-09-07 18:13:01 +0200

    fix attempt 4

--- src/db/collections/block.rs
+++ src/db/collections/block.rs
@@ -311,8 +311,7 @@ impl BlockCollection {
                     "_id": null,
                     "num_blocks": { "$count": {} },
                     "num_no_payload": { "$sum": {
-                        // "$cond": [ { "block.payload": { "$ne": null } }, 1 , 0 ]
-                        "$cond": [ { "$ne": [ "$block.payload", null ] }, 1 , 0 ]
+                        "$cond": [ { "$eq": [ "$block.payload", null ] }, 1 , 0 ]
                     } },
                 } },
                 // doc! { "$project": {
