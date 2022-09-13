5c697f30784bf5c26e93c0853781681b94235c90
Dr-Electron <dr-electr0n@protonmail.com>
2022-09-13 10:06:47 +0200

    docs(wiki): change Chronicle path to be compatible with other versions (#665)
    Use chronicle path to be compatible with other versions

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -7,7 +7,7 @@ module.exports = {
       {
         id: 'inx-chronicle-develop',
         path: path.resolve(__dirname, 'docs'),
-        routeBasePath: 'inx-chronicle',
+        routeBasePath: 'chronicle',
         sidebarPath: path.resolve(__dirname, 'sidebars.js'),
         editUrl: 'https://github.com/iotaledger/inx-chronicle/edit/main/documentation',
         remarkPlugins: [require('remark-code-import'), require('remark-import-partial')],
