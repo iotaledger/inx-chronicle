4bab57da387764aa277c6f86b5539fe64816cd5a
Jochen Görtler <grtlr@users.noreply.github.com>
2022-10-06 18:15:55 +0200

    refactor: remove actor framework (#777)
    * refactor: remove actor framework
    
    * Add shutdown handle
    
    * typo
    
    * PR suggestion
    
    * small changes
    
    * move around
    
    * clippy
    
    * you ain't gonna like that
    
    Co-authored-by: Alex Coats <alexandcoats@gmail.com>

---
+++ src/bin/inx-chronicle/error.rs
@@ -0,0 +1,22 @@
+// Copyright 2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use thiserror::Error;
+
+use crate::config::ConfigError;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    Config(#[from] ConfigError),
+    #[error(transparent)]
+    MongoDb(#[from] mongodb::error::Error),
+    #[cfg(feature = "api")]
+    #[error(transparent)]
+    Api(#[from] super::api::ApiError),
+    #[cfg(feature = "inx")]
+    #[error(transparent)]
+    Inx(#[from] super::stardust_inx::InxError),
+    #[error(transparent)]
+    Shutdown(#[from] tokio::sync::broadcast::error::SendError<()>),
+}
