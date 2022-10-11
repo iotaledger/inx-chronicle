3af58eafd046295c423156ddcf23e5d5388c8221
/alex/ <alexander.schmidt@iota.org>
2022-10-11 16:34:11 +0200

    fix(shutdown): fix shutdown logic (#800)
    * fix shutdown logic
    * simplify

--- src/bin/inx-chronicle/process.rs
+++ src/bin/inx-chronicle/process.rs
@@ -1,30 +1,18 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use tokio::{
-    signal::unix::{signal, SignalKind},
-    sync::mpsc,
-};
+use tokio::signal::unix::{signal, SignalKind};
 
-pub async fn interupt_or_terminate() -> mpsc::UnboundedReceiver<()> {
-    let (shutdown_send, shutdown_recv) = mpsc::unbounded_channel();
+pub async fn interrupt_or_terminate() {
+    let mut sigterm = signal(SignalKind::terminate()).expect("cannot listen to `SIGTERM`");
+    let mut sigint = signal(SignalKind::interrupt()).expect("cannot listen to `SIGINT`");
 
-    let _ = tokio::spawn(async move {
-        let mut sigterm = signal(SignalKind::terminate()).expect("cannot listen to `SIGTERM`");
-        let mut sigint = signal(SignalKind::interrupt()).expect("cannot listen to `SIGINT`");
-
-        tokio::select! {
-            _ = sigterm.recv() => {
-                tracing::info!("received `SIGTERM`, sending shutdown signal")
-            }
-            _ = sigint.recv() => {
-                tracing::info!("received `SIGTERM`, sending shutdown signal")
-            }
+    tokio::select! {
+        _ = sigterm.recv() => {
+            tracing::info!("received `SIGTERM`, sending shutdown signal")
         }
-
-        shutdown_send.send(()).expect("Could not send shutdown signal");
-    })
-    .await;
-
-    shutdown_recv
+        _ = sigint.recv() => {
+            tracing::info!("received `SIGTERM`, sending shutdown signal")
+        }
+    }
 }
