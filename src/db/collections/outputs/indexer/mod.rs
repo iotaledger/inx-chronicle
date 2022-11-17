6aff3148144cef24371a88700e1dd5cd9e746da4
Jochen Görtler <jochen.goertler@iota.org>
2022-11-17 10:55:23 +0100

    name

--- src/db/collections/outputs/indexer/mod.rs
+++ src/db/collections/outputs/indexer/mod.rs
@@ -381,7 +381,7 @@ impl OutputCollection {
                 .keys(doc! { "metadata.spent_metadata.spent.milestone_index": -1, "metadata.booked.milestone_index": -1 })
                 .options(
                     IndexOptions::builder()
-                        .name("output_spent_milestone_index".to_string())
+                        .name("output_spent_milestone_index_comp".to_string())
                         .build(),
                 )
                 .build(),
