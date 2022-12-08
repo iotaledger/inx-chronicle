0a8c841044e3604b5c1bb46b457f3fafa1bc743b
Jochen Görtler <grtlr@users.noreply.github.com>
2022-12-08 13:40:47 +0100

    feat(analytics): enable analytics selection in `fill-analytics` (#949)
    * Proof-of-concept `stardust_address_activity`
    
    * Convert more analytics
    
    * output activity
    
    * ledger size
    
    * block analytics
    
    * unlock conditions
    
    * protocol parameters
    
    * Cleanup
    
    * enum for Measurement
    
    * fmt
    
    * small fixes
    
    * test
    
    * Cleanup and choosing
    
    * fix more
    
    * concurrent get
    
    * fmt
    
    * async
    
    * Fix CI
    
    * uncomment loki
    
    * fmt
    
    * fix CI
    
    * Fix CI
    
    * pr feedback
    
    * missing comment
    
    Co-authored-by: Alexander Schmidt <alexander.schmidt@iota.org>

---
+++ src/db/collections/analytics/base_token.rs
@@ -0,0 +1,76 @@
+// Copyright 2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use async_trait::async_trait;
+use decimal::d128;
+use futures::TryStreamExt;
+use mongodb::{bson::doc, error::Error};
+use serde::{Deserialize, Serialize};
+
+use super::{Analytic, Measurement, PerMilestone};
+use crate::{
+    db::{collections::OutputCollection, MongoDb, MongoDbCollectionExt},
+    types::{stardust::milestone::MilestoneTimestamp, tangle::MilestoneIndex},
+};
+
+/// Computes the number of addresses that hold a balance.
+#[derive(Debug)]
+pub struct BaseTokenActivityAnalytics;
+
+#[derive(Copy, Clone, Debug, Default, PartialEq, Serialize, Deserialize)]
+pub struct BaseTokenActivityAnalyticsResult {
+    pub transferred_value: d128,
+}
+
+#[async_trait]
+impl Analytic for BaseTokenActivityAnalytics {
+    async fn get_measurement(
+        &mut self,
+        db: &MongoDb,
+        milestone_index: MilestoneIndex,
+        milestone_timestamp: MilestoneTimestamp,
+    ) -> Option<Result<Measurement, Error>> {
+        let res = db
+            .collection::<OutputCollection>()
+            .get_base_token_activity_analytics(milestone_index)
+            .await;
+        Some(match res {
+            Ok(measurement) => Ok(Measurement::BaseTokenActivity(PerMilestone {
+                milestone_index,
+                milestone_timestamp,
+                inner: measurement,
+            })),
+            Err(err) => Err(err),
+        })
+    }
+}
+
+impl OutputCollection {
+    /// Gathers output analytics.
+    #[tracing::instrument(skip(self), err, level = "trace")]
+    pub async fn get_base_token_activity_analytics(
+        &self,
+        milestone_index: MilestoneIndex,
+    ) -> Result<BaseTokenActivityAnalyticsResult, Error> {
+        Ok(self
+            .aggregate(
+                vec![
+                    doc! { "$match": {
+                        "metadata.booked.milestone_index": milestone_index,
+                    } },
+                    doc! { "$group" : {
+                        "_id": null,
+                        "transferred_value": { "$sum": { "$toDecimal": "$output.amount" } },
+                    } },
+                    doc! { "$project": {
+                        "transferred_value": { "$toString": "$transferred_value" },
+                    } },
+                ],
+                None,
+            )
+            .await?
+            .try_next()
+            .await?
+            .unwrap_or_default())
+    }
+}
