6ec481cc2200c7e7990770c2f20ace336bc0b3e7
Jochen Görtler <grtlr@users.noreply.github.com>
2022-12-20 10:41:05 +0100

    feat(refactor): add additional clippy lints (#989)

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 
 // Ideally, this would be handled completely by CI, but there is a bug in `petgraph` that prevents us from doing that.
 #![warn(missing_docs)]
+#![deny(unreachable_pub, private_in_public)]
 
 //! The basic types and MongoDb queries for Chronicle.
 
