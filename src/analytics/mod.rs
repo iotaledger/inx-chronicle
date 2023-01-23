d593edab49135ce49d91ccc89f6171c17382624c
Jochen Görtler <jochen.goertler@iota.org>
2023-01-23 13:35:44 +0100

    Wip

---
+++ src/analytics/mod.rs
@@ -0,0 +1,27 @@
+// Copyright 2023 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::types::{
+    ledger::BlockMetadata,
+    stardust::block::{Block, Output},
+    tangle::MilestoneIndex,
+};
+
+pub mod address_balance;
+pub mod block_activity;
+
+#[allow(missing_docs)]
+pub trait BlockAnalytics {
+    type Measurement;
+    fn begin_milestone(&mut self, index: MilestoneIndex);
+    fn handle_block(&mut self, block: &Block, block_metadata: &BlockMetadata, inputs: &Option<Vec<Output>>);
+    fn end_milestone(&mut self, index: MilestoneIndex) -> Option<Self::Measurement>;
+}
+
+#[allow(missing_docs)]
+pub trait TransactionAnalytics {
+    type Measurement;
+    fn begin_milestone(&mut self, index: MilestoneIndex);
+    fn handle_transaction(&mut self, inputs: &[Output], outputs: &[Output]);
+    fn end_milestone(&mut self, index: MilestoneIndex) -> Option<Self::Measurement>;
+}
