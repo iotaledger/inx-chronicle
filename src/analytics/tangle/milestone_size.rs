a8fa30a2b480261c642c3f4837a2b6b645087079
Alexander Schmidt <alexander.schmidt@iota.org>
2023-01-27 14:57:51 +0100

    usize instead of u64

--- src/analytics/tangle/milestone_size.rs
+++ src/analytics/tangle/milestone_size.rs
@@ -11,11 +11,11 @@ use crate::types::{
 /// Milestone size statistics.
 #[derive(Clone, Debug, Default)]
 pub struct MilestoneSizeMeasurement {
-    total_milestone_payload_bytes: u64,
-    total_tagged_data_payload_bytes: u64,
-    total_transaction_payload_bytes: u64,
-    total_treasury_transaction_payload_bytes: u64,
-    total_milestone_bytes: u64,
+    total_milestone_payload_bytes: usize,
+    total_tagged_data_payload_bytes: usize,
+    total_transaction_payload_bytes: usize,
+    total_treasury_transaction_payload_bytes: usize,
+    total_milestone_bytes: usize,
 }
 
 /// Computes the total and per-payload byte sizes for a given milestone.
@@ -31,13 +31,13 @@ impl BlockAnalytics for MilestoneSizeAnalytics {
     }
 
     fn handle_block(&mut self, block: &Block, raw_block: &[u8], _: &BlockMetadata) {
-        self.measurement.total_milestone_bytes += raw_block.len() as u64;
+        self.measurement.total_milestone_bytes += raw_block.len();
         match block.payload {
-            Some(Payload::Milestone(_)) => self.measurement.total_milestone_payload_bytes += raw_block.len() as u64,
-            Some(Payload::TaggedData(_)) => self.measurement.total_tagged_data_payload_bytes += raw_block.len() as u64,
-            Some(Payload::Transaction(_)) => self.measurement.total_transaction_payload_bytes += raw_block.len() as u64,
+            Some(Payload::Milestone(_)) => self.measurement.total_milestone_payload_bytes += raw_block.len(),
+            Some(Payload::TaggedData(_)) => self.measurement.total_tagged_data_payload_bytes += raw_block.len(),
+            Some(Payload::Transaction(_)) => self.measurement.total_transaction_payload_bytes += raw_block.len(),
             Some(Payload::TreasuryTransaction(_)) => {
-                self.measurement.total_treasury_transaction_payload_bytes += raw_block.len() as u64
+                self.measurement.total_treasury_transaction_payload_bytes += raw_block.len();
             }
             _ => {}
         }
