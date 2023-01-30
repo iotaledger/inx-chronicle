cc1e7ed1a46043d6aaca72662072d89aa6b39274
Alex Coats <alexandcoats@gmail.com>
2023-01-30 12:24:58 -0500

    impl milestone stream for mongo

--- src/tangle/sources/mongodb.rs
+++ src/tangle/sources/mongodb.rs
@@ -4,24 +4,78 @@
 use std::ops::RangeBounds;
 
 use async_trait::async_trait;
-use futures::{stream::BoxStream, TryStreamExt};
+use futures::{stream::BoxStream, StreamExt, TryStreamExt};
+use thiserror::Error;
 
 use super::{BlockData, InputSource, MilestoneData};
 use crate::{
-    db::{collections::BlockCollection, MongoDb},
+    db::{
+        collections::{BlockCollection, ConfigurationUpdateCollection, MilestoneCollection, ProtocolUpdateCollection},
+        MongoDb,
+    },
     tangle::ledger_updates::LedgerUpdateStore,
     types::tangle::MilestoneIndex,
 };
 
+#[derive(Debug, Error)]
+pub enum MongoDbInputSourceError {
+    #[error("missing milestone {0}")]
+    MissingMilestone(MilestoneIndex),
+    #[error("missing node config for ledger index {0}")]
+    MissingNodeConfig(MilestoneIndex),
+    #[error("missing protocol params for ledger index {0}")]
+    MissingProtocolParams(MilestoneIndex),
+    #[error(transparent)]
+    MongoDb(#[from] mongodb::error::Error),
+}
+
 #[async_trait]
 impl InputSource for MongoDb {
-    type Error = mongodb::error::Error;
+    type Error = MongoDbInputSourceError;
 
     async fn milestone_stream(
         &self,
-        _range: impl RangeBounds<MilestoneIndex> + Send,
+        range: impl RangeBounds<MilestoneIndex> + Send,
     ) -> Result<BoxStream<Result<MilestoneData, Self::Error>>, Self::Error> {
-        todo!()
+        use std::ops::Bound;
+        let start = match range.start_bound() {
+            Bound::Included(&idx) => idx.0,
+            Bound::Excluded(&idx) => idx.0 + 1,
+            Bound::Unbounded => 0,
+        };
+        let end = match range.end_bound() {
+            Bound::Included(&idx) => idx.0,
+            Bound::Excluded(&idx) => idx.0 - 1,
+            Bound::Unbounded => u32::MAX,
+        };
+        Ok(Box::pin(futures::stream::iter(start..=end).then(
+            move |index| async move {
+                let (milestone_id, at, payload) = self
+                    .collection::<MilestoneCollection>()
+                    .get_milestone(index.into())
+                    .await?
+                    .ok_or(MongoDbInputSourceError::MissingMilestone(index.into()))?;
+                let protocol_params = self
+                    .collection::<ProtocolUpdateCollection>()
+                    .get_protocol_parameters_for_ledger_index(index.into())
+                    .await?
+                    .ok_or(MongoDbInputSourceError::MissingProtocolParams(index.into()))?
+                    .parameters;
+                let node_config = self
+                    .collection::<ConfigurationUpdateCollection>()
+                    .get_node_configuration_for_ledger_index(index.into())
+                    .await?
+                    .ok_or(MongoDbInputSourceError::MissingNodeConfig(index.into()))?
+                    .config;
+                Ok(MilestoneData {
+                    milestone_id,
+                    at,
+                    payload,
+                    protocol_params,
+                    node_config,
+                })
+            },
+        )))
     }
 
     /// Retrieves a stream of blocks and their metadata in white-flag order given a milestone index.
@@ -33,6 +87,7 @@ impl InputSource for MongoDb {
             self.collection::<BlockCollection>()
                 .get_referenced_blocks_in_white_flag_order_stream(index)
                 .await?
+                .map_err(|e| e.into())
                 .map_ok(|(block_id, block, raw, metadata)| BlockData {
                     block_id,
                     block,
