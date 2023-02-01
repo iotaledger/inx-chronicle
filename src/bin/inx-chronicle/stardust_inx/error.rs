aafcfd2d020674fe0db02e24fb0b99952ef53b44
Jochen Görtler <jochen.goertler@iota.org>
2023-02-01 17:12:11 +0100

    revert error

--- src/bin/inx-chronicle/stardust_inx/error.rs
+++ src/bin/inx-chronicle/stardust_inx/error.rs
@@ -16,6 +16,15 @@ pub enum InxWorkerError {
     InfluxDb(#[from] influxdb::Error),
     #[error("expected INX address with format `http://<address>:<port>`, but found `{0}`")]
     InvalidAddress(String),
+    #[allow(unused)]
+    #[error("wrong number of ledger updates: `{received}` but expected `{expected}`")]
+    InvalidLedgerUpdateCount { received: usize, expected: usize },
+    #[allow(unused)]
+    #[error("invalid milestone state")]
+    InvalidMilestoneState,
+    #[allow(unused)]
+    #[error("missing milestone id for milestone index `{0}`")]
+    MissingMilestoneInfo(MilestoneIndex),
     #[error("MongoDb error: {0}")]
     MongoDb(#[from] mongodb::error::Error),
     #[error("network changed from previous run. old network name: `{0}`, new network name: `{1}`")]
