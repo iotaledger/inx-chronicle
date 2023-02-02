e65940922d7c609ae959470d062bbb210a1f120f
Alex Coats <alexandcoats@gmail.com>
2023-02-02 11:51:03 -0500

    ergonomics

--- src/bin/inx-chronicle/migrations/migrate_20230202.rs
+++ src/bin/inx-chronicle/migrations/migrate_20230202.rs
@@ -1,126 +1,122 @@
 // Copyright 2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use async_trait::async_trait;
 use chronicle::{
-    db::{
-        collections::{ApplicationStateCollection, OutputCollection},
-        MongoDb, MongoDbCollectionExt,
-    },
+    db::{collections::OutputCollection, MongoDb, MongoDbCollectionExt},
     types::stardust::block::output::{AliasId, NftId, OutputId},
 };
 use futures::TryStreamExt;
 use mongodb::{bson::doc, options::IndexOptions, IndexModel};
 use serde::Deserialize;
 
-pub const VERSION: &str = "20230202";
+use super::Migration;
 
-pub fn migrate(db: &MongoDb) -> eyre::Result<()> {
-    tracing::info!("Migrating to version {}", VERSION);
-    tokio::task::block_in_place(move || tokio::runtime::Handle::current().block_on(inner_migrate(db)))
-}
+pub struct Migrate;
 
-pub async fn inner_migrate(db: &MongoDb) -> eyre::Result<()> {
-    let collection = db.collection::<OutputCollection>();
+#[async_trait]
+impl Migration for Migrate {
+    const VERSION: &'static str = "20230202";
 
-    #[derive(Deserialize)]
-    struct Res {
-        output_id: OutputId,
-    }
+    async fn migrate(db: &MongoDb) -> eyre::Result<()> {
+        let collection = db.collection::<OutputCollection>();
 
-    // Convert the outputs with implicit IDs
-    let outputs = collection
-        .aggregate::<Res>(
-            [
-                doc! { "$match": { "$or": [
-                    { "output.alias_id": AliasId::implicit() },
-                    { "output.nft_id": NftId::implicit() }
-                ] } },
-                doc! { "$project": {
-                    "output_id": "$_id"
-                } },
-            ],
-            None,
-        )
-        .await?
-        .map_ok(|res| res.output_id)
-        .try_collect::<Vec<_>>()
-        .await?;
+        #[derive(Deserialize)]
+        struct Res {
+            output_id: OutputId,
+        }
 
-    for output_id in outputs {
-        // Alias and nft are the same length so both can be done this way since they are just serialized as bytes
-        let id = AliasId::from(output_id);
+        // Convert the outputs with implicit IDs
+        let outputs = collection
+            .aggregate::<Res>(
+                [
+                    doc! { "$match": { "$or": [
+                        { "output.alias_id": AliasId::implicit() },
+                        { "output.nft_id": NftId::implicit() }
+                    ] } },
+                    doc! { "$project": {
+                        "output_id": "$_id"
+                    } },
+                ],
+                None,
+            )
+            .await?
+            .map_ok(|res| res.output_id)
+            .try_collect::<Vec<_>>()
+            .await?;
+
+        for output_id in outputs {
+            // Alias and nft are the same length so both can be done this way since they are just serialized as bytes
+            let id = AliasId::from(output_id);
+            collection
+                .update_one(
+                    doc! { "_id": output_id },
+                    doc! { "$set": { "details.indexed_id": id } },
+                    None,
+                )
+                .await?;
+        }
+
+        // Get the outputs that don't have implicit IDs
         collection
-            .update_one(
-                doc! { "_id": output_id },
-                doc! { "$set": { "details.indexed_id": id } },
+            .update_many(
+                doc! {
+                    "output.kind": "alias",
+                    "output.alias_id": { "$ne": AliasId::implicit() },
+                },
+                vec![doc! { "$set": {
+                    "details.indexed_id": "$output.alias_id",
+                } }],
                 None,
             )
             .await?;
+
+        collection
+            .update_many(
+                doc! {
+                    "output.kind": "nft",
+                    "output.nft_id": { "$ne": NftId::implicit() },
+                },
+                vec![doc! { "$set": {
+                    "details.indexed_id": "$output.nft_id",
+                } }],
+                None,
+            )
+            .await?;
+
+        collection
+            .update_many(
+                doc! { "output.kind": "foundry" },
+                vec![doc! { "$set": {
+                    "details.indexed_id": "$output.foundry_id",
+                } }],
+                None,
+            )
+            .await?;
+
+        collection.drop_index("output_alias_id_index", None).await?;
+
+        collection.drop_index("output_foundry_id_index", None).await?;
+
+        collection.drop_index("output_nft_id_index", None).await?;
+
+        collection
+            .create_index(
+                IndexModel::builder()
+                    .keys(doc! { "details.indexed_id": 1 })
+                    .options(
+                        IndexOptions::builder()
+                            .name("output_indexed_id_index".to_string())
+                            .partial_filter_expression(doc! {
+                                "details.indexed_id": { "$exists": true },
+                            })
+                            .build(),
+                    )
+                    .build(),
+                None,
+            )
+            .await?;
+
+        Ok(())
     }
-
-    // Get the outputs that don't have implicit IDs
-    collection
-        .update_many(
-            doc! {
-                "output.kind": "alias",
-                "output.alias_id": { "$ne": AliasId::implicit() },
-            },
-            vec![doc! { "$set": {
-                "details.indexed_id": "$output.alias_id",
-            } }],
-            None,
-        )
-        .await?;
-
-    collection
-        .update_many(
-            doc! {
-                "output.kind": "nft",
-                "output.nft_id": { "$ne": NftId::implicit() },
-            },
-            vec![doc! { "$set": {
-                "details.indexed_id": "$output.nft_id",
-            } }],
-            None,
-        )
-        .await?;
-
-    collection
-        .update_many(
-            doc! { "output.kind": "foundry" },
-            vec![doc! { "$set": {
-                "details.indexed_id": "$output.foundry_id",
-            } }],
-            None,
-        )
-        .await?;
-
-    collection.drop_index("output_alias_id_index", None).await?;
-
-    collection.drop_index("output_foundry_id_index", None).await?;
-
-    collection.drop_index("output_nft_id_index", None).await?;
-
-    collection
-        .create_index(
-            IndexModel::builder()
-                .keys(doc! { "details.indexed_id": 1 })
-                .options(
-                    IndexOptions::builder()
-                        .name("output_indexed_id_index".to_string())
-                        .partial_filter_expression(doc! {
-                            "details.indexed_id": { "$exists": true },
-                        })
-                        .build(),
-                )
-                .build(),
-            None,
-        )
-        .await?;
-
-    db.collection::<ApplicationStateCollection>()
-        .set_last_migration(VERSION)
-        .await?;
-
-    Ok(())
 }
--- src/bin/inx-chronicle/migrations/mod.rs
+++ src/bin/inx-chronicle/migrations/mod.rs
@@ -3,32 +3,76 @@
 
 use std::collections::HashMap;
 
+use async_trait::async_trait;
 use chronicle::db::{collections::ApplicationStateCollection, MongoDb};
 use eyre::bail;
 
 pub mod migrate_20230202;
 
-type MigrateFn = fn(&MongoDb) -> eyre::Result<()>;
+pub const LATEST_VERSION: &str = migrate_20230202::Migrate::VERSION;
 
-pub const LATEST_VERSION: &str = migrate_20230202::VERSION;
+macro_rules! migration {
+    ($($mig:expr),*$(,)?) => {
+        migration([$(Box::new($mig) as _),*])
+    };
+}
 
-/// The list of migrations, specified as key-value tuples.
-/// - `key`: The `last_migration` value that can be migrated to the next iteration.
-/// - `value`: The migration fn pointer that migrates from `last_migration == key` to the next iteration.
-///
-/// In order to add a new migration, change the `LATEST_VERSION` above and add an entry to this list.
-///
-/// ## Example
-/// ```
-/// (Some(migrate_YYYYMMDD::VERSION), migrate_YYYYMMDD::migrate)
-/// ```
-const MIGRATIONS: [(Option<&str>, MigrateFn); 1] = [
-    // Initial migration
-    (None, migrate_20230202::migrate),
-];
+lazy_static::lazy_static! {
+    /// The list of migrations, in order.
+    ///
+    /// In order to add a new migration, change the `LATEST_VERSION` above and add an entry to this list
+    /// in the form `migrate_YYYYMMDD`.
+    static ref MIGRATIONS: HashMap<Option<&'static str>, Box<dyn DynMigration>> = migration![
+        migrate_20230202::Migrate,
+    ];
+}
+
+fn migration<const N: usize>(
+    migrations: [Box<dyn DynMigration>; N],
+) -> HashMap<Option<&'static str>, Box<dyn DynMigration>> {
+    let mut map = HashMap::default();
+    let mut prev_version = None;
+    for migration in migrations {
+        let version = migration.version();
+        map.insert(prev_version, migration);
+        prev_version = Some(version);
+    }
+    map
+}
+
+#[async_trait]
+trait Migration {
+    const VERSION: &'static str;
+
+    async fn migrate(db: &MongoDb) -> eyre::Result<()>;
+}
+
+trait DynMigration: Send + Sync {
+    fn version(&self) -> &'static str;
+
+    fn migrate(&self, db: &MongoDb) -> eyre::Result<()>;
+}
+
+impl<T: Migration + Send + Sync> DynMigration for T {
+    fn version(&self) -> &'static str {
+        T::VERSION
+    }
+
+    fn migrate(&self, db: &MongoDb) -> eyre::Result<()> {
+        tracing::info!("Migrating to version {}", T::VERSION);
+        tokio::task::block_in_place(move || {
+            tokio::runtime::Handle::current().block_on(async {
+                T::migrate(db).await?;
+                db.collection::<ApplicationStateCollection>()
+                    .set_last_migration(T::VERSION)
+                    .await?;
+                Ok(())
+            })
+        })
+    }
+}
 
 pub async fn migrate(db: &MongoDb) -> eyre::Result<()> {
-    let migrations = HashMap::from(MIGRATIONS);
     loop {
         let last_migration = db
             .collection::<ApplicationStateCollection>()
@@ -37,9 +81,9 @@ pub async fn migrate(db: &MongoDb) -> eyre::Result<()> {
         if matches!(last_migration.as_deref(), Some(LATEST_VERSION)) {
             break;
         }
-        match migrations.get(&last_migration.as_deref()) {
-            Some(migrate) => {
-                (migrate)(db)?;
+        match MIGRATIONS.get(&last_migration.as_deref()) {
+            Some(migration) => {
+                migration.migrate(db)?;
             }
             None => {
                 bail!("cannot migrate version {:?}", last_migration);
