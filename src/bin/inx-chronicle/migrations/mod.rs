77c35b82ddbfb3aa9dc1223c3fa4c4b8427cf0f4
Alex Coats <alexandcoats@gmail.com>
2023-02-02 08:59:25 -0500

    suggestions

--- src/bin/inx-chronicle/migrations/mod.rs
+++ src/bin/inx-chronicle/migrations/mod.rs
@@ -6,18 +6,23 @@ use eyre::bail;
 
 pub mod migrate_20230202;
 
+const LATEST_VERSION: &'static str = "20230202";
+
 pub async fn migrate(db: &MongoDb) -> eyre::Result<()> {
-    let last_migration = db
-        .collection::<ApplicationStateCollection>()
-        .get_application_state()
-        .await?
-        .map(|s| s.last_migration);
-    match last_migration.as_deref() {
-        // First migration using the method, so there is no current version
-        None => {
-            migrate_20230202::migrate(db).await?;
+    loop {
+        let last_migration = db
+            .collection::<ApplicationStateCollection>()
+            .get_application_state()
+            .await?
+            .map(|s| s.last_migration);
+        match last_migration.as_deref() {
+            // First migration using the method, so there is no current version
+            None => {
+                migrate_20230202::migrate(db).await?;
+            }
+            Some(LATEST_VERSION) => break,
+            Some(v) => bail!("cannot migrate version {}", v),
         }
-        Some(v) => bail!("cannot migrate version {}", v),
     }
     Ok(())
 }
