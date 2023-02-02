477fe9910b47a83531c747262174477508161943
Alex Coats <alexandcoats@gmail.com>
2023-02-02 10:08:04 -0500

    doc

--- src/bin/inx-chronicle/migrations/mod.rs
+++ src/bin/inx-chronicle/migrations/mod.rs
@@ -12,6 +12,16 @@ type MigrateFn = fn(&MongoDb) -> eyre::Result<()>;
 
 pub const LATEST_VERSION: &str = migrate_20230202::VERSION;
 
+/// The list of migrations, specified as key-value tuples.
+/// - `key`: The `last_migration` value that can be migrated to the next iteration.
+/// - `value`: The migration fn pointer that migrates from `last_migration == key` to the next iteration.
+///
+/// In order to add a new migration, change the `LATEST_VERSION` above and add an entry to this list.
+///
+/// ## Example
+/// ```
+/// (Some(migrate_YYYYMMDD::VERSION), migrate_YYYYMMDD::migrate)
+/// ```
 const MIGRATIONS: [(Option<&str>, MigrateFn); 1] = [
     // Initial migration
     (None, migrate_20230202::migrate),
