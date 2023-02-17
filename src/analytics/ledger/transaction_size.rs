07d6745f2d2b62297b53fbbb87c6c394c748fff2
Alexander Schmidt <alexander.schmidt@iota.org>
2023-02-17 12:51:10 +0100

    Set up buckets only once

--- src/analytics/ledger/transaction_size.rs
+++ src/analytics/ledger/transaction_size.rs
@@ -1,68 +1,84 @@
 // Copyright 2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-#![allow(missing_docs)]
-
 use std::{collections::HashMap, fmt::Display};
+
+use once_cell::sync::OnceCell;
 
 use super::*;
 
+fn get_empty_buckets() -> HashMap<Bucket, usize> {
+    static EMPTY_BUCKETS: OnceCell<HashMap<Bucket, usize>> = OnceCell::new();
+
+    EMPTY_BUCKETS
+        .get_or_init(|| {
+            let mut buckets = HashMap::from([
+                (Bucket::Small, 0),
+                (Bucket::Medium, 0),
+                (Bucket::Large, 0),
+                (Bucket::Huge, 0),
+            ]);
+            for i in 1..=7 {
+                buckets.insert(Bucket::Single(i), 0);
+            }
+            buckets
+        })
+        .clone()
+}
+
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
-pub(crate) enum SizeBucket {
-    Single(usize), // 1,..7
+pub(crate) enum Bucket {
+    Single(usize), // 1,..,7
     Small,         // [8..16)
     Medium,        // [16..32)
     Large,         // [32..64)
     Huge,          // [64..128)
 }
 
-impl From<usize> for SizeBucket {
+impl From<usize> for Bucket {
     fn from(value: usize) -> Self {
         match value {
             0 => unreachable!("invalid transaction"),
-            1..=7 => SizeBucket::Single(value),
-            8..=15 => SizeBucket::Small,
-            16..=31 => SizeBucket::Medium,
-            32..=63 => SizeBucket::Large,
-            _ => SizeBucket::Huge,
+            1..=7 => Bucket::Single(value),
+            8..=15 => Bucket::Small,
+            16..=31 => Bucket::Medium,
+            32..=63 => Bucket::Large,
+            _ => Bucket::Huge,
         }
     }
 }
 
-impl Display for SizeBucket {
+impl Display for Bucket {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            SizeBucket::Single(v) => write!(f, "{}", v),
-            SizeBucket::Small => write!(f, "small"),
-            SizeBucket::Medium => write!(f, "medium"),
-            SizeBucket::Large => write!(f, "large"),
-            SizeBucket::Huge => write!(f, "huge"),
+            Bucket::Single(v) => write!(f, "{}", v),
+            Bucket::Small => write!(f, "small"),
+            Bucket::Medium => write!(f, "medium"),
+            Bucket::Large => write!(f, "large"),
+            Bucket::Huge => write!(f, "huge"),
         }
     }
 }
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug)]
 pub(crate) struct TransactionSizeMeasurement {
-    pub(crate) input_buckets: HashMap<SizeBucket, usize>,
-    pub(crate) output_buckets: HashMap<SizeBucket, usize>,
+    pub(crate) input_buckets: HashMap<Bucket, usize>,
+    pub(crate) output_buckets: HashMap<Bucket, usize>,
+}
+
+impl Default for TransactionSizeMeasurement {
+    fn default() -> Self {
+        Self {
+            input_buckets: get_empty_buckets(),
+            output_buckets: get_empty_buckets(),
+        }
+    }
 }
 
 impl Analytics for TransactionSizeMeasurement {
     type Measurement = TransactionSizeMeasurement;
 
-    fn begin_milestone(&mut self, _ctx: &dyn AnalyticsContext) {
-        let mut buckets = HashMap::from([
-            (SizeBucket::Small, 0),
-            (SizeBucket::Medium, 0),
-            (SizeBucket::Large, 0),
-            (SizeBucket::Huge, 0),
-        ]);
-        for i in 0..8 {
-            buckets.insert(SizeBucket::Single(i), 0);
-        }
-        self.input_buckets = buckets.clone();
-        self.output_buckets = buckets;
-    }
+    fn begin_milestone(&mut self, _ctx: &dyn AnalyticsContext) {}
 
     fn handle_transaction(&mut self, consumed: &[LedgerSpent], created: &[LedgerOutput], _ctx: &dyn AnalyticsContext) {
         *self.input_buckets.entry(consumed.len().into()).or_default() += 1;
