a83161e1cdb30fa5b2a7272e731a35a4ae0e3387
Alex Coats <alexandcoats@gmail.com>
2023-03-17 13:17:01 -0400

    Fix children query

--- src/db/mongodb/collections/block.rs
+++ src/db/mongodb/collections/block.rs
@@ -206,18 +206,18 @@ impl BlockCollection {
             .aggregate(
                 [
                     doc! { "$match": {
-                        "metadata.referenced_by_milestone_index": { "$gte": block_referenced_index },
-                        "metadata.referenced_by_milestone_index": { "$lte": max_referenced_index },
+                        "metadata.referenced_by_milestone_index": {
+                            "$gte": block_referenced_index,
+                            "$lte": max_referenced_index
+                        },
                         "block.parents": block_id,
                     } },
-                    doc! { "$skip": (page_size * page) as i64 },
                     doc! { "$sort": {"metadata.referenced_by_milestone_index": -1} },
+                    doc! { "$skip": (page_size * page) as i64 },
                     doc! { "$limit": page_size as i64 },
                     doc! { "$project": { "_id": 1 } },
                 ],
-                AggregateOptions::builder()
-                    .hint(Hint::Name("block_referenced_index_comp".to_owned()))
-                    .build(),
+                None,
             )
             .await?
             .map_ok(|BlockIdResult { block_id }| block_id))
