1e3212f0e6b2d9c35dad2c675b1f4447f90c71c7
Alexander Schmidt <alexander.schmidt@iota.org>
2023-03-20 16:16:00 +0100

    improve

--- src/bin/inx-chronicle/migrations/mod.rs
+++ src/bin/inx-chronicle/migrations/mod.rs
@@ -118,10 +118,11 @@ pub async fn migrate(db: &MongoDb) -> eyre::Result<()> {
                 migration.migrate(db).await?;
             }
             None => {
-                bail!(
-                    "cannot migrate version {:?}, database is in invalid state",
-                    last_migration.unwrap_or(0)
-                );
+                if let Some(id) = last_migration {
+                    bail!("cannot migrate from version `{id}`; database is in invalid state");
+                } else {
+                    bail!("migration failure; database is in invalid state");
+                }
             }
         }
     }
