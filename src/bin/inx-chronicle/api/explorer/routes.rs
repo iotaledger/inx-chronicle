50391e9f36e35a732807735a5eee6f5b424a8281
shufps <shufps80@gmail.com>
2023-11-21 09:13:14 +0100

    used the cache as mutex to prevent concurrent refreshs

--- src/bin/inx-chronicle/api/explorer/routes.rs
+++ src/bin/inx-chronicle/api/explorer/routes.rs
@@ -351,7 +351,7 @@ async fn richest_addresses_ledger_analytics(
     RichestAddressesQuery { top, ledger_index }: RichestAddressesQuery,
 ) -> ApiResult<RichestAddressesResponse> {
     let ledger_index = resolve_ledger_index(&database, ledger_index).await?;
-    let cache = RICHEST_ADDRESSES_CACHE.read().await;
+    let cache = RICHEST_ADDRESSES_CACHE.write().await;
     let seconds_until_midnight = calculate_seconds_until_midnight();
 
     if let Some(cached_data) = &*cache {
@@ -359,8 +359,6 @@ async fn richest_addresses_ledger_analytics(
             return Ok(cached_data.data.clone());
         }
     }
-
-    drop(cache); // release the read lock
 
     info!("refreshing richest-addresses cache ...");
     let refresh_start = SystemTime::now();
@@ -411,7 +409,7 @@ async fn token_distribution_ledger_analytics(
     LedgerIndex { ledger_index }: LedgerIndex,
 ) -> ApiResult<TokenDistributionResponse> {
     let ledger_index = resolve_ledger_index(&database, ledger_index).await?;
-    let cache = TOKEN_DISTRIBUTION_CACHE.read().await;
+    let cache = TOKEN_DISTRIBUTION_CACHE.write().await;
 
     let seconds_until_midnight = calculate_seconds_until_midnight();
     if let Some(cached_data) = &*cache {
@@ -419,8 +417,6 @@ async fn token_distribution_ledger_analytics(
             return Ok(cached_data.data.clone());
         }
     }
-
-    drop(cache); // release the read lock
 
     info!("refreshing token-distribution cache ...");
     let refresh_start = SystemTime::now();
