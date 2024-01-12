d9a3aa9bf7134fa01605a25dab3451551ee9ecc9
Alex Coats <alexandcoats@gmail.com>
2024-01-12 10:10:46 -0500

    optimized migration

--- src/bin/inx-chronicle/migrations/migrate_2.rs
+++ src/bin/inx-chronicle/migrations/migrate_2.rs
@@ -4,12 +4,18 @@
 use async_trait::async_trait;
 use chronicle::{
     db::{
-        mongodb::collections::{LedgerUpdateCollection, MilestoneCollection, OutputCollection},
-        MongoDb, MongoDbCollection,
+        mongodb::collections::{LedgerUpdateCollection, OutputCollection},
+        MongoDb, MongoDbCollection, MongoDbCollectionExt,
     },
-    tangle::LedgerUpdateStore,
+    model::{
+        ledger::{LedgerOutput, LedgerSpent, RentStructureBytes},
+        metadata::OutputMetadata,
+        utxo::{Output, OutputId},
+    },
 };
-use futures::prelude::stream::TryStreamExt;
+use futures::{prelude::stream::TryStreamExt, StreamExt};
+use mongodb::bson::doc;
+use serde::Deserialize;
 use tokio::{task::JoinSet, try_join};
 
 use super::Migration;
@@ -22,73 +28,95 @@ pub struct Migrate;
 impl Migration for Migrate {
     const ID: usize = 2;
     const APP_VERSION: &'static str = "1.0.0-rc.3";
-    const DATE: time::Date = time::macros::date!(2024 - 01 - 09);
+    const DATE: time::Date = time::macros::date!(2024 - 01 - 12);
 
     async fn migrate(db: &MongoDb) -> eyre::Result<()> {
-        let start_index = db.collection::<MilestoneCollection>().get_oldest_milestone().await?;
-        let end_index = db.collection::<MilestoneCollection>().get_newest_milestone().await?;
+        db.collection::<LedgerUpdateCollection>()
+            .collection()
+            .drop(None)
+            .await?;
 
-        if let (Some(start_index), Some(end_index)) = (start_index, end_index) {
-            if end_index.milestone_index > start_index.milestone_index {
-                // Drop the ledger updates before we rebuild them
-                db.collection::<LedgerUpdateCollection>()
-                    .collection()
-                    .drop(None)
-                    .await?;
+        let outputs_stream = db
+            .collection::<OutputCollection>()
+            .find::<OutputDocument>(doc! {}, None)
+            .await?;
+        let mut batched_stream = outputs_stream.try_chunks(INSERT_BATCH_SIZE);
 
-                // Restore the ledger updates using output data
-                for index in start_index.milestone_index.0..=end_index.milestone_index.0 {
-                    let consumed = db
-                        .collection::<OutputCollection>()
-                        .get_consumed_outputs(index.into())
-                        .await?
-                        .try_collect()
-                        .await?;
+        let mut tasks = JoinSet::new();
 
-                    let created = db
-                        .collection::<OutputCollection>()
-                        .get_created_outputs(index.into())
-                        .await?
-                        .try_collect()
-                        .await?;
-
-                    let ledger_updates = LedgerUpdateStore::init(consumed, created);
-
-                    let mut tasks = JoinSet::new();
-
-                    for batch in ledger_updates.created_outputs().chunks(INSERT_BATCH_SIZE) {
-                        let db = db.clone();
-                        let batch = batch.to_vec();
-                        tasks.spawn(async move {
-                            db.collection::<LedgerUpdateCollection>()
-                                .insert_unspent_ledger_updates(&batch)
-                                .await
-                        });
-                    }
-
-                    for batch in ledger_updates.consumed_outputs().chunks(INSERT_BATCH_SIZE) {
-                        let db = db.clone();
-                        let batch = batch.to_vec();
-                        tasks.spawn(async move {
-                            try_join! {
-                                async {
-                                    db.collection::<OutputCollection>().update_spent_outputs(&batch).await?;
-                                    Ok(())
-                                },
-                                async {
-                                    db.collection::<LedgerUpdateCollection>().insert_spent_ledger_updates(&batch).await?;
-                                    Ok(())
-                                }
-                            }.and(Ok(()))
-                        });
-                    }
-
-                    while let Some(res) = tasks.join_next().await {
-                        res??;
-                    }
+        while let Some(batch) = batched_stream.next().await {
+            let batch = batch?;
+            while tasks.len() >= 100 {
+                if let Some(res) = tasks.join_next().await {
+                    res??;
                 }
             }
+            let db = db.clone();
+            tasks.spawn(async move {
+                let consumed = batch.iter().filter_map(Option::<LedgerSpent>::from).collect::<Vec<_>>();
+                let created = batch.into_iter().map(LedgerOutput::from).collect::<Vec<_>>();
+                try_join! {
+                    async {
+                        db.collection::<LedgerUpdateCollection>()
+                                .insert_unspent_ledger_updates(&created)
+                                .await
+                    },
+                    async {
+                        db.collection::<OutputCollection>().update_spent_outputs(&consumed).await
+                    },
+                    async {
+                        db.collection::<LedgerUpdateCollection>().insert_spent_ledger_updates(&consumed).await
+                    }
+                }
+                .and(Ok(()))
+            });
         }
+
+        while let Some(res) = tasks.join_next().await {
+            res??;
+        }
+
         Ok(())
+    }
+}
+
+#[derive(Deserialize)]
+pub struct OutputDocument {
+    #[serde(rename = "_id")]
+    output_id: OutputId,
+    output: Output,
+    metadata: OutputMetadata,
+    details: OutputDetails,
+}
+
+#[derive(Deserialize)]
+struct OutputDetails {
+    rent_structure: RentStructureBytes,
+}
+
+impl From<OutputDocument> for LedgerOutput {
+    fn from(value: OutputDocument) -> Self {
+        Self {
+            output_id: value.output_id,
+            block_id: value.metadata.block_id,
+            booked: value.metadata.booked,
+            output: value.output,
+            rent_structure: value.details.rent_structure,
+        }
+    }
+}
+
+impl From<&OutputDocument> for Option<LedgerSpent> {
+    fn from(value: &OutputDocument) -> Self {
+        value.metadata.spent_metadata.map(|spent_metadata| LedgerSpent {
+            spent_metadata,
+            output: LedgerOutput {
+                output_id: value.output_id,
+                block_id: value.metadata.block_id,
+                booked: value.metadata.booked,
+                output: value.output.clone(),
+                rent_structure: value.details.rent_structure,
+            },
+        })
     }
 }
